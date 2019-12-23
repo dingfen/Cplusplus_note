b9cea541a8b6202ade3c0e631dbccb8650b61d90
dingfen <df12138@mail.ustc.edu.cn>
2019-12-23 21:48:37 +0800

    add reference on Dec 23 2019

---
+++ src/basic/reference/reference.cpp
@@ -0,0 +1,54 @@
+typedef int&  lref;
+typedef int&& rref;
+int n;
+lref&  r1 = n; // type of r1 is int&
+lref&& r2 = n; // type of r2 is int&
+rref&  r3 = n; // type of r3 is int&
+rref&& r4 = 1; // type of r4 is int&&
+
+#include <iostream>
+#include <string>
+ 
+
+void case1() {
+    std::string s = "Ex";
+    std::string& r1 = s;
+    const std::string& r2 = s;
+ 
+    r1 += "ample";           // modifies s
+//  r2 += "!";               // error: cannot modify through reference to const
+    std::cout << r2 << '\n'; // prints s, which now holds "Example"
+}
+
+
+char& char_number(std::string& s, std::size_t n) {
+    return s.at(n); // string::at() returns a reference to char
+}
+
+// When a function's return type is lvalue reference, 
+// the function call expression becomes an lvalue expression:
+void case2() {
+    std::string str = "Test";
+    char_number(str, 1) = 'a'; // the function call is lvalue, can be assigned to
+    std::cout << str << '\n';
+}
+
+
+void case3() {
+    std::string s1 = "Test";
+//  std::string&& r1 = s1;           // error: can't bind to lvalue
+ 
+    const std::string& r2 = s1 + s1; // okay: lvalue reference to const extends lifetime
+//  r2 += "Test";                    // error: can't modify through reference to const
+ 
+    std::string&& r3 = s1 + s1;      // okay: rvalue reference extends lifetime
+    r3 += "Test";                    // okay: can modify through reference to non-const
+    std::cout << r3 << '\n';
+}
+
+
+int main() {
+    case1();
+    case2();
+    case3();
+}
